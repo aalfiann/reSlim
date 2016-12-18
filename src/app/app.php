45202449da00f45ef7faf3971b1d68a3699c58f9
aalfiann@gmail.com <aalfiann@gmail.com>
2016-12-18 17:32:01 +0700

    Restructure for easy development

---
+++ src/app/app.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * reSlim is based on Slim Framework version 3.6 (http://slimframework.com)
+ *
+ * @link      https://github.com/aalfiann/reSlim
+ * @copyright Copyright (c) 2016 M ABD AZIZ ALFIAN
+ * @license   https://github.com/iSlim3/license.md (MIT License)
+ */
+
+// Load all class libraries
+require '../vendor/autoload.php';
+// Load config
+require '../config.php';
+
+// Set up dependencies
+require __DIR__.'/dependencies.php';
+
+// Set up middleware
+require __DIR__.'/middleware.php';
+
+// Load all router files before run
+$routers = glob('../routers/*.router.php');
+foreach ($routers as $router) {
+    require $router;
+}
+
+$app->run();
+
+?>
