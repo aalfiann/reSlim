6179d22f71561654df0360043330cc064b422e2f
aziz alfian <aziz alfian>
2017-04-17 21:06:54 +0700

    Change new icon

--- test/example/global-meta.php
+++ test/example/global-meta.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8" />
-	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/tkd.ico">
-	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/tkd.ico">
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/reslim.ico">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/reslim.ico">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
