6335881facc32d0b237d4b0b631dd0fbebd73370
aziz alfian <aziz alfian>
2017-04-17 21:13:50 +0700

    refactor structure request and add new request for handle user upload management

--- src/routers/user.router.php
+++ src/routers/user.router.php
@@ -31,7 +31,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
     });
 
     // GET example api to show all data user with pagination
-    $app->get('/user/{page}/{itemsperpage}/{token}', function (Request $request, Response $response) {
+    $app->get('/user/data/{page}/{itemsperpage}/{token}', function (Request $request, Response $response) {
         $users = new classes\User($this->db);
         $users->page = $request->getAttribute('page');
         $users->itemsPerPage = $request->getAttribute('itemsperpage');
@@ -47,7 +47,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
     });
 
     // GET example api to search data user with pagination
-    $app->get('/user/search/{query}/{page}/{itemsperpage}/{token}', function (Request $request, Response $response) {
+    $app->get('/user/data/search/{query}/{page}/{itemsperpage}/{token}', function (Request $request, Response $response) {
         $users = new classes\User($this->db);
         $users->search = $request->getAttribute('query');
         $users->page = $request->getAttribute('page');
@@ -257,3 +257,139 @@ use \Psr\Http\Message\ResponseInterface as Response;
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withBody($body);
     });
+
+    // POST example api upload
+    $app->post('/user/upload', function (Request $request, Response $response) {
+        $upload = new classes\Upload($this->db);
+        
+        $datapost = $request->getParsedBody();
+        $files = $request->getUploadedFiles();
+        if (empty($files['Datafile'])) {
+            throw new Exception('Expected a newfile');
+        }
+        
+        $upload->username = $datapost['Username'];
+        $upload->title = $datapost['Title'];
+        $upload->alternate = $datapost['Alternate'];
+        $upload->externallink = $datapost['External'];
+        $upload->datafile = $files['Datafile'];
+        $upload->token = $datapost['Token'];
+        $upload->baseurl = $request->getUri()->getBaseUrl();
+        
+        $body = $response->getBody();
+        $body->write($upload->process());
+        return $response
+            ->withStatus(200)
+            ->withHeader('Content-Type','application/json; charset=utf-8')
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withBody($body);
+    });
+
+    // POST example api update user upload item
+    $app->post('/user/upload/update', function (Request $request, Response $response) {
+        $upload = new classes\Upload($this->db);
+        $datapost = $request->getParsedBody();
+        
+        $upload->username = $datapost['Username'];
+        $upload->title = $datapost['Title'];
+        $upload->alternate = $datapost['Alternate'];
+        $upload->externallink = $datapost['External'];
+        $upload->status = $datapost['Status'];
+        $upload->itemid = $datapost['ItemID'];
+        $upload->token = $datapost['Token'];
+
+        $body = $response->getBody();
+        $body->write($upload->update());
+        return $response
+            ->withStatus(200)
+            ->withHeader('Content-Type','application/json; charset=utf-8')
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withBody($body);
+    });
+
+    // POST example api delete user upload item
+    $app->post('/user/upload/delete', function (Request $request, Response $response) {
+        $upload = new classes\Upload($this->db);
+        $datapost = $request->getParsedBody();
+        
+        $upload->username = $datapost['Username'];
+        $upload->itemid = $datapost['ItemID'];
+        $upload->token = $datapost['Token'];
+
+        $body = $response->getBody();
+        $body->write($upload->delete());
+        return $response
+            ->withStatus(200)
+            ->withHeader('Content-Type','application/json; charset=utf-8')
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withBody($body);
+    });
+
+    // GET example api to show all data status for upload
+    $app->get('/user/upload/status/{token}', function (Request $request, Response $response) {
+        $upload = new classes\Upload($this->db);
+        $upload->token = $request->getAttribute('token');
+        $body = $response->getBody();
+        $body->write($upload->showOptionStatus());
+        return $response
+            ->withStatus(200)
+            ->withHeader('Content-Type','application/json; charset=utf-8')
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withBody($body);
+    });
+
+    // GET example api to show all data user upload with pagination
+    $app->get('/user/{username}/upload/data/{page}/{itemsperpage}/{token}', function (Request $request, Response $response) {
+        $upload = new classes\Upload($this->db);
+        $upload->page = $request->getAttribute('page');
+        $upload->itemsPerPage = $request->getAttribute('itemsperpage');
+        $upload->token = $request->getAttribute('token');
+        $upload->username = $request->getAttribute('username');
+        $body = $response->getBody();
+        $body->write($upload->showAllAsPagination());
+        return $response
+            ->withStatus(200)
+            ->withHeader('Content-Type','application/json; charset=utf-8')
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withBody($body);
+    });
+
+    // GET example api to search all data user upload with pagination
+    $app->get('/user/{username}/upload/data/search/{query}/{page}/{itemsperpage}/{token}', function (Request $request, Response $response) {
+        $upload = new classes\Upload($this->db);
+        $upload->page = $request->getAttribute('page');
+        $upload->itemsPerPage = $request->getAttribute('itemsperpage');
+        $upload->token = $request->getAttribute('token');
+        $upload->username = $request->getAttribute('username');
+        $upload->search = $request->getAttribute('query');
+        $body = $response->getBody();
+        $body->write($upload->searchAllAsPagination());
+        return $response
+            ->withStatus(200)
+            ->withHeader('Content-Type','application/json; charset=utf-8')
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withBody($body);
+    });
+
+    // GET example api to show user upload item (doesn't need a authentication)
+    $app->get('/user/{username}/upload/data/item/{itemid}', function (Request $request, Response $response) {
+        $upload = new classes\Upload($this->db);
+        $upload->username = $request->getAttribute('username');
+        $upload->itemid = $request->getAttribute('itemid');
+        $body = $response->getBody();
+        $body->write($upload->showItem());
+        return $response
+            ->withStatus(200)
+            ->withHeader('Content-Type','application/json; charset=utf-8')
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withBody($body);
+    });
+
+    
