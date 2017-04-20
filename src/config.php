caa8e85dc972fb86bcb958a8aabf161c460b108e
aziz alfian <aziz alfian>
2017-04-20 18:29:25 +0700

    Add default name from

--- src/config.php
+++ src/config.php
@@ -33,6 +33,7 @@ $config['db']['dbname'] = 'reSlim';
  * @var $config['smtp']['auth'] will connect to smtp using authentication
  * @var $config['smtp']['secure'] this is type of smtp security. You can use tls or ssl
  * @var $config['smtp']['port'] this is port smtp
+ * @var $config['smtp']['defaultnamefrom'] this is default name from. You can filled with yourname / yourwebsitetitle
  * @var $config['smtp']['username'] your username to login into smtp server
  * @var $config['smtp']['password'] the password to login into smtp server
  * @var $config['smtp']['debug'] get more information by set debug.
@@ -44,6 +45,7 @@ $config['smtp']['autotls'] = false;
 $config['smtp']['auth'] = true;
 $config['smtp']['secure'] = 'tls';
 $config['smtp']['port'] = 587;
+$config['smtp']['defaultnamefrom'] = 'reSlim admin';
 $config['smtp']['username'] = 'youremail@gmail.com';
 $config['smtp']['password'] = 'secret';
 $config['smtp']['debug'] = 1;
