60c4b594be34f2551d1cf0fdc7c4912c1d66defc
aziz alfian <aziz alfian>
2017-04-27 16:26:23 +0700

    - Add security key - Add checkbox for agree terms of service - Add modals for show terms of service

--- test/example/tab-register.php
+++ test/example/tab-register.php
@@ -3,26 +3,41 @@
                     <div class="row">
                         <div class="col-md-7 col-sm-3 col-md-offset-1 col-sm-offset-2">
                         <?php
+                            $aaa=rand(0,5);$bbb=rand(3,9);
                             if (isset($_POST['submitregister']))
                             {
-                                if ($_POST['password1'] == $_POST['password2']){
-                                    $post_array = array(
-                                    	'Username' => $_POST['username'],
-                                    	'Password' => $_POST['password2'],
-                                        'Fullname' => $_POST['fullname'],
-                                        'Address' => $_POST['address'],
-                                        'Phone' => $_POST['phone'],
-                                        'Email' => $_POST['email'],
-                                        'Aboutme' => $_POST['aboutme'],
-                                        'Avatar' => $_POST['avatar'],
-                                        'Role' => '3'
-                                    );
-                                    Core::register(Core::$api.'/user/register',$post_array);
+                                if (!empty($_POST['agree'])){
+                                    if (($_POST['aaa'] + $_POST['bbb']) == $_POST['key']){
+                                        if ($_POST['password1'] == $_POST['password2']){
+                                            $post_array = array(
+                                            	'Username' => $_POST['username'],
+                                            	'Password' => $_POST['password2'],
+                                                'Fullname' => $_POST['fullname'],
+                                                'Address' => $_POST['address'],
+                                                'Phone' => $_POST['phone'],
+                                                'Email' => $_POST['email'],
+                                                'Aboutme' => $_POST['aboutme'],
+                                                'Avatar' => $_POST['avatar'],
+                                                'Role' => '3'
+                                            );
+                                            Core::register(Core::$api.'/user/register',$post_array);
+                                        } else {
+                                            echo '<div class="alert alert-danger" role="alert">
+                                                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                <strong>Process Register Failed,</strong> Your password is not match! 
+                                            </div>';
+                                        }
+                                    } else {
+                                        echo '<div class="alert alert-danger" role="alert">
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                            <strong>Process Register Failed,</strong> Wrong security key! 
+                                        </div>';
+                                    }
                                 } else {
                                     echo '<div class="alert alert-danger" role="alert">
-                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                            <strong>Process Register Failed,</strong> Your password is not match! 
-                                        </div>';
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <strong>Process Register Failed,</strong> You are not agree with the terms of service! 
+                                    </div>';
                                 }
                             } 
                         ?>
@@ -69,6 +84,15 @@
                                             <label>Avatar</label>
                                             <input name="avatar" type="text" placeholder="Please input url image for Your Avatar." class="form-control border-input">
                                         </div>
+                                        <div class="form-group">
+                                            <label>Security Key: </label>
+                                            <b><?=$aaa?> + <?=$bbb?> = ?</b><input name="key" type="text" placeholder="Please answer this question" class="form-control border-input" maxlength="15" required>
+                                            <input type="text" name="aaa" value="<?=$aaa?>" hidden>
+            								<input type="text" name="bbb" value="<?=$bbb?>" hidden>
+                                        </div>
+                                        <label class="checkbox" for="checkbox1">
+	                                	    <input name="agree" type="checkbox" id="checkbox1" data-toggle="checkbox">I agree to the <a href="#" data-toggle="modal" data-target="#termsofservice">terms of service</a>
+	                                	</label>
                                         <hr>
                                         <div class="form-group text-center">
                                             <button name="submitregister" type="submit" class="btn btn-fill btn-wd ">Register</button>
@@ -76,6 +100,44 @@
                                     </div>
                                 </div>
                             </form>
+                            <!-- Start Modal Terms of Service -->
+                            <div class="modal fade" id="termsofservice" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                            <h4 class="modal-title" id="myModalLabel">Terms Of Service</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                        <p>You agree, through your use of this service, that you will not use this
+application to post any material which is knowingly false and/or defamatory,
+inaccurate, abusive, vulgar, hateful, harassing, obscene, profane, sexually
+oriented, threatening, invasive of a person's privacy, or otherwise violative
+of any law. You agree not to post any copyrighted material unless the
+copyright is owned by you.</p>
+
+<p>We as owner of this application also reserve the right to reveal your identity (or
+whatever information we know about you) in the event of a complaint or legal
+action arising from any message posted by you. We log all internet protocol
+addresses accessing this web site.</p>
+
+<p>Please note that advertisements, chain letters, pyramid schemes, and
+solicitations are inappropriate on this application.</p>
+
+<p>We reserve the right to remove any content for any reason or no reason at
+all. We reserve the right to terminate any membership for any reason or no
+reason at all.</p>
+
+<p>You must be at least 13 years of age to use this service.</p>
+
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Modal Terms of Service -->
                         </div>
                     </div>
                 </div>
