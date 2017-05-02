f72dbaa34f7b9141976bd9e3dd935887c4dc399c
aziz alfian <aziz alfian>
2017-05-02 18:23:39 +0700

    Add enableApiKeys

--- src/config.php
+++ src/config.php
@@ -5,11 +5,13 @@
  * @var $config['displayErrorDetails'] to display error details on slim
  * @var $config['addContentLengthHeader'] to set the Content-Length header which makes Slim behave more predictably
  * @var $config['limitLoadData'] to protect high request data load. Default is 1000.
+ * @var $config['enableApiKeys'] to protect api from guest or anonymous. Guest which don't have api key can not using this service. Default is true.
  * 
  */
 $config['displayErrorDetails']      = true;
 $config['addContentLengthHeader']   = false;
 $config['limitLoadData'] = 1000;
+$config['enableApiKeys'] = true;
 
 /** 
  * Configuration PDO MySQL Database
