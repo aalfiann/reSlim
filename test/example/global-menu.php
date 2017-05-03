4e2403e8f4ea971451160621540491259bda336a
aziz alfian <aziz alfian>
2017-05-03 18:59:24 +0700

    - Add menu API Keys and Settings

--- test/example/global-menu.php
+++ test/example/global-menu.php
@@ -1,16 +1,15 @@
 <div class="sidebar-wrapper">
             <div class="logo">
-                <a href="<?=Core::$basepath;?>" class="simple-text">
-                    reSlim
+                <a href="<?php echo Core::getInstance()->basepath;?>" class="simple-text">
+                    <?php echo Core::getInstance()->title?>
                 </a>
-                <div class="text-center">v.1.2.0</div>
+                <div class="text-center">v.<?php echo Core::getInstance()->version?></div>
             </div>
 
             <ul class="nav">
             <?php if (!empty($datalogin['username'])) 
             { ?>
-                <?php // SuperUser and Admin Menu 
-                    if (Core::getRole($datalogin['token']) != 3) { ?>
+                <?php if (Core::getRole($datalogin['token']) != 3) { // SuperUser and Admin Menu ?>
                     <li <?php if (!empty($_GET['m'])) if($_GET['m']==3) echo 'class="active"';?> >
                         <a href="modul-dashboard.php?m=3">
                             <i class="ti-panel"></i>
@@ -25,7 +24,7 @@
                     </li>
                     <li <?php if (!empty($_GET['m'])) if($_GET['m']==5) echo 'class="active"';?> >
                         <a href="modul-data-user.php?m=5&page=1&itemsperpage=10">
-                            <i class="ti-pencil"></i>
+                            <i class="ti-id-badge"></i>
                             <p>Data User</p>
                         </a>
                     </li>
@@ -35,6 +34,19 @@
                             <p>Explore</p>
                         </a>
                     </li>
+                    <li <?php if (!empty($_GET['m'])) if($_GET['m']==7) echo 'class="active"';?> >
+                        <a href="modul-data-api.php?m=7&page=1&itemsperpage=10&search=">
+                            <i class="ti-lock"></i>
+                            <p>API Keys</p>
+                        </a>
+                    </li>
+                    <?php if (Core::getRole($datalogin['token']) == 1) { // SuperUser Only ?>
+                        <li <?php if (!empty($_GET['m'])) if($_GET['m']==8) echo 'class="active"';?> >
+                            <a href="modul-settings.php?m=8">
+                                <i class="ti-settings"></i>
+                                <p>Settings</p>
+                            </a>
+                        </li><?php } ?>
                 <?php } else {  // Member Menu ?>
                     <li <?php if (!empty($_GET['m'])) if($_GET['m']==4) echo 'class="active"';?> >
                         <a href="modul-user-profile.php?m=4">
@@ -46,6 +58,12 @@
                         <a href="modul-explore.php?m=6&page=1&itemsperpage=12&search=">
                             <i class="ti-cloud-up"></i>
                             <p>Explore</p>
+                        </a>
+                    </li>
+                    <li <?php if (!empty($_GET['m'])) if($_GET['m']==7) echo 'class="active"';?> >
+                        <a href="modul-data-api.php?m=7&page=1&itemsperpage=10&search=">
+                            <i class="ti-lock"></i>
+                            <p>API Keys</p>
                         </a>
                     </li>
                 <?php } ?>
