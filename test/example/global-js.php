366fe2f2865a76485a5de221e6fafa3b6d956d83
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2017-11-12 02:39:49 +0700

    - Defer globaljs for pagespeed load - Add undefer version globaljs - Update version to 1.6.1

--- test/example/global-js.php
+++ test/example/global-js.php
@@ -1,65 +1,84 @@
-	<!-- LazySizes -->
-	<script src="assets/js/lazysizes.min.js" async=""></script>
-	
-	<!--   Core JS Files   -->
-    <script src="assets/js/jquery-1.10.2.min.js" type="text/javascript"></script>
-	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+<!-- Defer Javascript -->
+<script>
+(function() {
+	function getScript(url,success){
+		var script=document.createElement('script');
+		script.src=url;
+		var head=document.getElementsByTagName('head')[0],
+		done=false;
+		script.onload=script.onreadystatechange = function(){
+			if ( !done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete') ) {
+				done=true;
+				success();
+				script.onload = script.onreadystatechange = null;
+				head.removeChild(script);
+			}
+		};
+		head.appendChild(script);
+	  }
 
-	<!--  Checkbox, Radio & Switch Plugins -->
-	<script src="assets/js/bootstrap-checkbox-radio.min.js"></script>
+	// Load LazySizes
+	getScript('assets/js/lazysizes.min.js',function(){});
 
-	<!-- Paper Dashboard Core javascript and methods for Demo purpose -->
-	<script src="assets/js/paper-dashboard.min.js"></script>
-
-	<!-- Export -->
-	<script type="text/javascript" src="assets/js/package.export.js"></script>
-	<script type="text/javascript" src="https://code.jquery.com/ui/1.10.2/jquery-ui.min.js"></script>
-	<script>$("head").append("<link rel='stylesheet' type='text/css' href='https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css' /><style>.ui-autocomplete{max-height:200px;overflow-y:auto;overflow-x:hidden}* html .ui-autocomplete{height:200px} .ui-autocomplete-loading { background:url('assets/img/ajax-loader.gif') no-repeat right center }</style><style>.lazyload {opacity: 0;} .lazyloading {opacity: 1;transition: opacity 300ms;background: #f7f7f7 url(assets/img/blank.gif) no-repeat center;} .sidebar .nav > li.active-bottom{position:fixed;width:100%;bottom:10px;} .sidebar .nav > li.active-bottom a{background:rgba(255,255,255,0.14);opacity:1;color:#FFFFFF;}</style><style>.modal{overflow:auto;min-height:100%;position:absolute;background-color:#000000;opacity:0.95 !important;} body.modal-open .main-panel{overflow:hidden !important;}</style>");</script>
-	<script type="text/javascript">
-    $(function(){
-        $("#firstdate").datepicker({
-            dateFormat:"yy-mm-dd"
-        }),
-        $("#lastdate").datepicker({
-            dateFormat:"yy-mm-dd"
-        }),
-		$('#formUpload').submit(function() {
-			$.ajax({
-				url : $(this).attr("action"),
-				type: "POST",
-				data : new FormData(this),
-				contentType: false,
-				cache: false,
-				processData:false,
-				xhr: function(){
-					//upload Progress
-					var xhr = $.ajaxSettings.xhr();
-					if (xhr.upload) {
-						xhr.upload.addEventListener('progress', function(event) {
-							var percent = 0;
-							var position = event.loaded || event.position;
-							var total = event.total;
-							if (event.lengthComputable) {
-								percent = Math.ceil(position / total * 100);
-							}
-							/*update progressbar for jQuery UI
-							$('#statusprogress').text(percent +"%");
-							$( '#progressbar' ).progressbar({
-								value: percent
-							});*/
-							//update progressbar for Twitter Bootstrap
-							$('#statusprogress').text(percent +"%");
-							$('#progressbar').css('width', percent+'%').attr('aria-valuenow', percent); 
-						}, true);
-					}
-					return xhr;
-				},
-				mimeType:"multipart/form-data"
-			}).done(function(res){ //
-				//$(my_form_id)[0].reset(); //reset form
-				//$(result_output).html(res); //output response from server
-				//submit_btn.val("Upload").prop( "disabled", false); //enable submit button once ajax is done
+	// Load jQuery
+	getScript('assets/js/jquery-1.10.2.min.js',function(){
+		// Load Bootstrap
+		getScript('assets/js/bootstrap.min.js',function(){
+			// Load Checkbox, Radio & Switch Plugins
+			getScript('assets/js/bootstrap-checkbox-radio.min.js',function(){});
+			// Load Paper Dashboard Core javascript and methods for Demo purpose
+			getScript('assets/js/paper-dashboard.min.js',function(){});
+			// Load Export
+			getScript('assets/js/package.export.js',function(){});
+			// Load jQuery UI
+			getScript('assets/js/jquery-ui-1.10.2.min.js',function(){
+				$("head").append("<link rel='stylesheet' type='text/css' href='assets/css/jquery-ui-theme-1.10.1.min.css' /><style>.ui-autocomplete{max-height:200px;overflow-y:auto;overflow-x:hidden}* html .ui-autocomplete{height:200px} .ui-autocomplete-loading { background:url('assets/img/ajax-loader.gif') no-repeat right center }</style><style>.lazyload {opacity: 0;} .lazyloading {opacity: 1;transition: opacity 300ms;background: #f7f7f7 url(assets/img/blank.gif) no-repeat center;} .sidebar .nav > li.active-bottom{position:fixed;width:100%;bottom:10px;} .sidebar .nav > li.active-bottom a{background:rgba(255,255,255,0.14);opacity:1;color:#FFFFFF;}</style><style>.modal{overflow:auto;min-height:100%;position:absolute;background-color:#000000;opacity:0.95 !important;} body.modal-open .main-panel{overflow:hidden !important;}</style>");
+				$("#firstdate").datepicker({
+					dateFormat:"yy-mm-dd"
+				}),
+				$("#lastdate").datepicker({
+					dateFormat:"yy-mm-dd"
+				});
+			});
+			$('#formUpload').submit(function() {
+				$.ajax({
+					url : $(this).attr("action"),
+					type: "POST",
+					data : new FormData(this),
+					contentType: false,
+					cache: false,
+					processData:false,
+					xhr: function(){
+						//upload Progress
+						var xhr = $.ajaxSettings.xhr();
+						if (xhr.upload) {
+							xhr.upload.addEventListener('progress', function(event) {
+								var percent = 0;
+								var position = event.loaded || event.position;
+								var total = event.total;
+								if (event.lengthComputable) {
+									percent = Math.ceil(position / total * 100);
+								}
+								/*update progressbar for jQuery UI
+								$('#statusprogress').text(percent +"%");
+								$( '#progressbar' ).progressbar({
+									value: percent
+								});*/
+								//update progressbar for Twitter Bootstrap
+								$('#statusprogress').text(percent +"%");
+								$('#progressbar').css('width', percent+'%').attr('aria-valuenow', percent); 
+							}, true);
+						}
+						return xhr;
+					},
+					mimeType:"multipart/form-data"
+				}).done(function(res){ //
+					//$(my_form_id)[0].reset(); //reset form
+					//$(result_output).html(res); //output response from server
+					//submit_btn.val("Upload").prop( "disabled", false); //enable submit button once ajax is done
+				});
 			});
 		});
-    });
-	</script>
+	});	
+})();
+</script>
---
+++ test/example/global-js.undefer.php
@@ -0,0 +1,65 @@
+	<!-- LazySizes -->
+	<script src="assets/js/lazysizes.min.js" async=""></script>
+	
+	<!--   Core JS Files   -->
+    <script src="assets/js/jquery-1.10.2.min.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+	<!--  Checkbox, Radio & Switch Plugins -->
+	<script src="assets/js/bootstrap-checkbox-radio.min.js"></script>
+
+	<!-- Paper Dashboard Core javascript and methods for Demo purpose -->
+	<script src="assets/js/paper-dashboard.min.js"></script>
+
+	<!-- Export -->
+	<script type="text/javascript" src="assets/js/package.export.js"></script>
+	<script type="text/javascript" src="https://code.jquery.com/ui/1.10.2/jquery-ui.min.js"></script>
+	<script>$("head").append("<link rel='stylesheet' type='text/css' href='https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css' /><style>.ui-autocomplete{max-height:200px;overflow-y:auto;overflow-x:hidden}* html .ui-autocomplete{height:200px} .ui-autocomplete-loading { background:url('assets/img/ajax-loader.gif') no-repeat right center }</style><style>.lazyload {opacity: 0;} .lazyloading {opacity: 1;transition: opacity 300ms;background: #f7f7f7 url(assets/img/blank.gif) no-repeat center;} .sidebar .nav > li.active-bottom{position:fixed;width:100%;bottom:10px;} .sidebar .nav > li.active-bottom a{background:rgba(255,255,255,0.14);opacity:1;color:#FFFFFF;}</style><style>.modal{overflow:auto;min-height:100%;position:absolute;background-color:#000000;opacity:0.95 !important;} body.modal-open .main-panel{overflow:hidden !important;}</style>");</script>
+	<script type="text/javascript">
+    $(function(){
+        $("#firstdate").datepicker({
+            dateFormat:"yy-mm-dd"
+        }),
+        $("#lastdate").datepicker({
+            dateFormat:"yy-mm-dd"
+        }),
+		$('#formUpload').submit(function() {
+			$.ajax({
+				url : $(this).attr("action"),
+				type: "POST",
+				data : new FormData(this),
+				contentType: false,
+				cache: false,
+				processData:false,
+				xhr: function(){
+					//upload Progress
+					var xhr = $.ajaxSettings.xhr();
+					if (xhr.upload) {
+						xhr.upload.addEventListener('progress', function(event) {
+							var percent = 0;
+							var position = event.loaded || event.position;
+							var total = event.total;
+							if (event.lengthComputable) {
+								percent = Math.ceil(position / total * 100);
+							}
+							/*update progressbar for jQuery UI
+							$('#statusprogress').text(percent +"%");
+							$( '#progressbar' ).progressbar({
+								value: percent
+							});*/
+							//update progressbar for Twitter Bootstrap
+							$('#statusprogress').text(percent +"%");
+							$('#progressbar').css('width', percent+'%').attr('aria-valuenow', percent); 
+						}, true);
+					}
+					return xhr;
+				},
+				mimeType:"multipart/form-data"
+			}).done(function(res){ //
+				//$(my_form_id)[0].reset(); //reset form
+				//$(result_output).html(res); //output response from server
+				//submit_btn.val("Upload").prop( "disabled", false); //enable submit button once ajax is done
+			});
+		});
+    });
+	</script>
