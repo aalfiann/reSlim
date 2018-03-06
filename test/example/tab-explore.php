d2eb6c990d28bf115dec46d912f4e61809280641
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-03-06 13:44:47 +0700

    - Fixed data status upload

--- test/example/tab-explore.php
+++ test/example/tab-explore.php
@@ -129,7 +129,7 @@ $itemsperpage = filter_var((empty($_GET['itemsperpage'])?'12':$_GET['itemsperpag
     $data = json_decode(Core::execGetRequest($url));
 
     // Data Status
-    $urlstatus = Core::getInstance()->api.'/user/status/'.$datalogin['token'];
+    $urlstatus = Core::getInstance()->api.'/user/upload/status/'.$datalogin['token'];
     $datastatus = json_decode(Core::execGetRequest($urlstatus));
 
     if (!empty($data))
