2151bcc9b36168d966f37a5d53e81d6377a92175
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-04-24 11:46:22 +0700

    - Update comments

--- src/classes/middleware/ApiKey.php
+++ src/classes/middleware/ApiKey.php
@@ -34,6 +34,15 @@ use PDO;
             $this->conf = $config['enableApiKeys'];
         }
 
+        /**
+         * ApiKey middleware invokable class
+         * 
+         * @param \Psr\Http\Message\ServerRequestInterface  $request    PSR7 request
+         * @param \Psr\Http\Message\ResponseInterface       $response   PSR7 response
+         * @param callable                                  $next       Next middleware
+         * 
+         * @return \Psr\Http\Message\ResponseInterface
+         */
         public function __invoke($request, $response, $next){
             if ($this->conf == true){
                 if (!empty($this->apikey)){
