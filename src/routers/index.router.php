6a7a325fbae458e3dd1f9cdba2f5f2f49125b401
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-05-07 15:40:12 +0700

    - Update index router

--- src/routers/index.router.php
+++ src/routers/index.router.php
@@ -2,6 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \classes\SimpleCache as SimpleCache;
+use \classes\JSON as JSON;
 
     // GET example api to show all data role
     $app->get('/', function (Request $request, Response $response) {
@@ -13,17 +14,16 @@ use \classes\SimpleCache as SimpleCache;
             $data = [
                 'status' => 'success',
 			    'code' => '200',
-    			'welcome' => 'Hello World, here is the default index reSlim',
+                'welcome' => 'Hello World, here is the default index reSlim',
                 'author' => [
                     'name' => 'M ABD AZIZ ALFIAN (aalfiann@gmail.com)',
                     'github' => 'https://github.com/aalfian/reSlim',
                     'license' => 'https://github.com/aalfiann/reSlim/blob/master/license.md'
                 ],
-                'how_to_use' => 'reSlim is using authentication by token. So You have to register and login to get generated new token.',
-                'generate_time' => date('Y-m-d h:i:s a', time())
+                'how_to_use' => 'reSlim is using authentication by token. So You have to register and login to get generated new token.'
             ];
             $blacklistparam = ["&_=","&query=","&search=","token","apikey","api_key","time","timestamp","time_stamp","etag","key","q","s","k","t"];
-            $datajson = SimpleCache::save(json_encode($data, JSON_PRETTY_PRINT),null,$blacklistparam);
+            $datajson = SimpleCache::save(JSON::encode($data, true),null,$blacklistparam);
         }
         $body->write($datajson);
         return classes\Cors::modify($response,$body,200);
