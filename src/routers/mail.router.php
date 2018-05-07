e47a0716b6322766b051b0436cddb001baba6053
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-05-07 15:41:03 +0700

    - Add new send mail with default config in router

--- src/routers/mail.router.php
+++ src/routers/mail.router.php
@@ -24,3 +24,25 @@ use \classes\middleware\ValidateParam as ValidateParam;
     })->add(new ValidateParam(['FromName','CC','BCC','Attachment']))
         ->add(new ValidateParam(['To','Subject','Message','Html'],'','required'))
         ->add(new ValidateParam('From','6-50','email'));
+        
+
+    // POST example api to send mail with default email
+    $app->post('/mail/send/default', function (Request $request, Response $response) {      
+        $mail = new classes\Mailer($this->mail);
+        $datapost = $request->getParsedBody();
+
+        $mail->addAddress = $datapost['To'];
+        $mail->subject = $datapost['Subject'];
+        $mail->body = $datapost['Message'];
+        $mail->isHtml = $datapost['Html'];
+        $mail->setFrom = $this->settings['smtp']['username'];
+        $mail->setFromName = $this->settings['smtp']['defaultnamefrom'];
+        $mail->addCC = $datapost['CC'];
+        $mail->addBCC = $datapost['BCC'];
+        $mail->addAttachment = $datapost['Attachment'];
+        
+        $body = $response->getBody();
+        $body->write($mail->send());
+        return classes\Cors::modify($response,$body,200);
+    })->add(new ValidateParam(['CC','BCC','Attachment']))
+        ->add(new ValidateParam(['To','Subject','Message','Html'],'','required'));
