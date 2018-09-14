32cb46e46d952e8faf657f68efa8c5c28ea4f5f8
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-09-14 19:52:45 +0700

    - Add router for test the cache transfer - Update version to 1.17.1 - Update readme

--- src/routers/maintenance.router.php
+++ src/routers/maintenance.router.php
@@ -201,3 +201,31 @@ use \classes\JSON as JSON;
         $body->write(JSON::encode(UniversalCache::listenToDeleteSingleKey($secretkey,$keycache,$agecache),true));
         return classes\Cors::modify($response,$body,200);
     });
+
+    // Filebased cache data test
+    $app->get('/maintenance/cache/data/transfer/test', function (Request $request, Response $response) {
+        $body = $response->getBody();
+        $content = 'Now check the data cache file inside folder cache!';
+        SimpleCache::save('{"data":"'.$content.'"}');
+        $body->write(JSON::encode(['send' =>$content],true));
+        return classes\Cors::modify($response,$body,200);
+    });
+
+    // Filebased cache apikey test
+    $app->get('/maintenance/cache/apikey/transfer/test', function (Request $request, Response $response) {
+        $body = $response->getBody();
+        $content = 'Now check the data cache file inside folder cache-keys!';
+        Auth::writeCache(str_replace([' ','!'],'-',$content));
+        $body->write(JSON::encode(['send' =>$content],true));
+        return classes\Cors::modify($response,$body,200);
+    });
+
+    // Filebased cache universal test
+    $app->get('/maintenance/cache/universal/transfer/test', function (Request $request, Response $response) {
+        $body = $response->getBody();
+        $key = 'test_universal_cache_transfer';
+        $content = 'Now check the data cache file inside folder cache-universal! Filename is encoded.';
+        UniversalCache::writeCache($key,$content);
+        $body->write(JSON::encode(['send' =>$content],true));
+        return classes\Cors::modify($response,$body,200);
+    });
